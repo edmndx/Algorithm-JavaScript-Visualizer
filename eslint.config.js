4f7e83a006b38ec86e361a3b0e0b19696446a7e4
edmundasx <edmundaskaminskas@live.com>
2026-07-17 01:27:29 +0300

    Initialize feature-oriented project architecture

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import babelParser from '@babel/eslint-parser';
+import eslint from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['dist', 'coverage'] },
+  eslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          plugins: ['@babel/plugin-syntax-jsx'],
+          presets: [['@babel/preset-typescript', { ignoreExtensions: true }]],
+        },
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'no-undef': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
